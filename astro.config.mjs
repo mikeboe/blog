105b0e7d3cd326bf43405edb58710ec35da18190
mikeboe <boehrermichael@gmail.com>
2024-06-04 09:32:33 +0200

    Add Dockerfile and GitHub Actions workflow for publishing website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mikeboe.com',
 	integrations: [mdx(), sitemap()],
 });
