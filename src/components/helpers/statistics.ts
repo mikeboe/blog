7df84c83c96fd94e370f73c997040a6ed228c9f4
mikeboe <boehrermichael@gmail.com>
2024-06-05 22:24:43 +0200

    refactor: Include date prop in Card component and update PostList
    This commit refactors the Card component to include a new prop called "date", allowing for dynamic rendering of the date in the card's time element. Additionally, the PostList component has been updated, replacing the previous implementation and improving the structure and organization of the codebase.

---
+++ src/components/helpers/statistics.ts
@@ -0,0 +1,101 @@
+import { Octokit, App } from "octokit";
+import fs from "fs";
+import colors from "./colors.json"
+
+
+const getStats = async () => {
+  const octokit = new Octokit({
+    auth: import.meta.env.GH_TOKEN,
+  });
+
+  const data = await octokit
+    .request("GET /user/repos", {
+      per_page: 100,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
+
+  let languagesData = <{}>[];
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].full_name)
+    await octokit.rest.repos
+      .listLanguages({
+        owner: data[i].owner.login,
+        repo: data[i].name,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+      .then((response) => {
+        cumulateLanguages(response.data)
+       
+        // sleep for 0,5s
+        console.log(i + "/" + data.length + " done");
+      });
+  }
+
+  function cumulateLanguages(data) {
+    for (let key in data) {
+      
+      const index = languagesData.findIndex((item) => item.name === key)
+     
+
+      if(index !== -1) {
+        
+        languagesData[index].value += data[key]
+
+      } else {
+        languagesData.push({
+          name: key,
+          value: data[key]
+        })
+      }
+    }
+  }
+
+  const totalSize = languagesData.reduce((acc, item) => acc + item.value, 0)
+
+  languagesData = languagesData.map((item) => {
+    return {
+      name: item.name,
+      value: item.value,
+      percentage: ((item.value / totalSize) * 100).toFixed(2),
+      colour: colors[item.name].color
+    }
+  })
+
+  languagesData.sort((a, b) => b.value - a.value)
+
+  
+
+  console.log(languagesData)
+
+  const dataToSave = {
+    languages: languagesData,
+    totalSize,
+    date: new Date(),
+  };
+  
+
+  // save the data to a file
+  fs.writeFileSync("data.json", JSON.stringify(dataToSave, null, 2));
+
+  /* await octokit.request('GET /repos/{owner}/{repo}/languages', {
+      owner: 'rcktsftwr',
+      repo: 'rcktcrm',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    }).then((response) => {
+      console.log(response.data);
+    }) */
+};
+
+getStats().catch((error) => {
+  console.error(error);
+});
